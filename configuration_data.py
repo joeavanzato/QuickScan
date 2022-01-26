ea8b73a96db4aa577fa1ea002d18026842f4d021
panscan <joeavanzato@gmail.com>
2022-01-25 20:25:34 -0500

    Minor Update

--- configuration_data.py
+++ configuration_data.py
@@ -2,5 +2,5 @@
 
 detection_csv = ""
 fields = []
-bad_extensions = ["ps1", "bat", "dll", "psm1", "psd1", "cmd", "js", "scr", "cmd", "lnk", "jar", "hta", "vba", "vbs"]
+bad_extensions = ["ps1", "bat", "dll", "psm1", "psd1", "cmd", "js", "scr", "cmd", "lnk", "jar", "hta", "vba", "vbs", "exe"]
 
