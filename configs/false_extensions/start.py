10051389801311f9e4e187327f2d5a7bbf73381a
panscan <joeavanzato@gmail.com>
2022-01-26 20:06:42 -0500

    Correcting logic for file with space vs extension.  Minimizing paths utilized.

--- configs/false_extensions/start.py
+++ configs/false_extensions/start.py
@@ -42,24 +42,26 @@ def extension_scan():
         #        print(p)
         #        allow_list.append(p)
     homedir = os.getenv("HOMEDRIVE")
-    for root, sub, f in os.walk(homedir+"\\"):
-        for file in f:
-            name, extension = os.path.splitext(file)
-            extension = extension.replace(".", "").lower()
-            if extension in configuration_data.bad_extensions and extension.endswith(" "):# in ext_data['extensions'] and not root in allow_list:
-                detection_base = {}
-                full_path = os.path.join(root, file)
-                print(f"Found Suspicious File Extension with Trailing Space: {full_path}")
-                detection_base['Name'] = "Extension with Trailing Space"
-                detection_base['Reason'] = "Malware and Threat Actors often use false extensions to disguise the true OS-interpreted extension used at runtime."
-                detection_base['File Path'] = full_path
-                detection_base['Registry Path'] = "NA"
-                detection_base['MITRE Tactic'] = "Execution"
-                detection_base['MITRE Technique'] = "T1036.006"
-                detection_base['Risk'] = "High"
-                detection_base['Details'] = "NA"
-                detection_list.append(detection_base)
-                matches.append(full_path)
+    scan_paths = [homedir+"\\Users", homedir+"\\temp", homedir+"\\ProgramData", os.getenv("SYSTEMROOT")+"\\System32",os.getenv("SYSTEMROOT")+"\\syswow64"]
+    for path in scan_paths:
+        for root, sub, f in os.walk(path):
+            for file in f:
+                name, extension = os.path.splitext(file)
+                extension = extension.replace(".", "").lower()
+                if name.endswith(" "):# in ext_data['extensions'] and not root in allow_list:
+                    detection_base = {}
+                    full_path = os.path.join(root, file)
+                    print(f"Found Suspicious File Name with Trailing Space: {full_path}")
+                    detection_base['Name'] = "File Name with Trailing Space"
+                    detection_base['Reason'] = "Malware and Threat Actors often use false extensions to disguise the true OS-interpreted extension used at runtime."
+                    detection_base['File Path'] = full_path
+                    detection_base['Registry Path'] = "NA"
+                    detection_base['MITRE Tactic'] = "Execution"
+                    detection_base['MITRE Technique'] = "T1036.006"
+                    detection_base['Risk'] = "High"
+                    detection_base['Details'] = "NA"
+                    detection_list.append(detection_base)
+                    matches.append(full_path)
     logging.info(str(datetime.datetime.now()) + " False Extension Scan Complete")
     helpers.write_detection.write_detection(configuration_data.detection_csv, configuration_data.fields, detection_list)
     return path_list
